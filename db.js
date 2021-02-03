16e306e8b0555b134737847d1a7efb9748d25875
arhamumar94 <arhamumar1999@gmail.com>
2021-02-04 00:13:28 +0530

    hey

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize=require('sequelize')
 
 const db= new Sequelize('shopdb','shopper','shoppass',{
     host:'localhost',
-    dialect:'mysql',
+    dialect:'sqlite',
     pool:{
         min:0,
         max:5,
